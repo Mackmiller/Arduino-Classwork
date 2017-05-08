6e28fa01fda953bc1d4c10626915dbe03ae580ad
Mackenzie Miller <mackmiller19@gmail.com>
2017-05-08 06:12:41 -0400

    last upload

---
+++ Week Eleven/readme.md
@@ -0,0 +1,5 @@
+#serial
+
+Attached is the code that I figured out to get two LEDs working through the serial connection to the arduino. The toughest part for me was getting the hand of how serial communication worked.
+
+Link to video/code: https://drive.google.com/open?id=0B1wwFJOwtbwjTUd3Wm9VYkF5Rmc
--- Week Eleven/week11/week11.ino
+++ Week Eleven/week11/week11.ino
@@ -1,13 +1,13 @@
  char val; // Data received from the serial port
- int ledPin = 8; // Set the pin to digital I/O 13
+ int ledPin = 8; 
  int ledPin2 = 12;
 
 void setup() {
    pinMode(ledPin, OUTPUT); 
    pinMode(ledPin2, OUTPUT); 
    
-   // Set pin as OUTPUT
-   Serial.begin(9600); // Start serial communication at 9600 bps
+   /
+   Serial.begin(9600); 
 }
 
 void loop() {
@@ -15,16 +15,16 @@ void loop() {
 
 if (Serial.available()) 
    { // If data is available to read,
-     val = Serial.read(); // read it and store it in val
+     val = Serial.read(); 
    }
    if (val == '1') 
    { // If 1 was received
      digitalWrite(ledPin, HIGH);
-     digitalWrite(ledPin2, HIGH);// turn the LED on
+     digitalWrite(ledPin2, HIGH);
    } else {
      digitalWrite(ledPin, LOW);
      digitalWrite(ledPin2, LOW);
-     // otherwise turn it off
+ 
    }
-   delay(10); // Wait 10 milliseconds for next reading
+   delay(10); 
 }
